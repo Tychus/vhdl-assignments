0ec75c96fd54dde06d465ce9fc527741dfae9f7b
Igor Suleymanoff <igorsuleymanoff@gmail.com>
2015-01-27 23:27:10 +0200

    fix: fix #6 simulation

--- assignment_6/multiplier_tb.vhd
+++ assignment_6/multiplier_tb.vhd
@@ -8,8 +8,10 @@ entity multiplier_tb is
 end multiplier_tb;
 
 architecture testbench of multiplier_tb is
-  signal test : std_logic_vector(7 downto 0):=(others => '0');
+  signal test_1 : std_logic_vector(3 downto 0) := (others => '0');
+  signal test_2 : std_logic_vector(3 downto 0) := (others => '0');
   signal testres: std_logic_vector(7 downto 0);
+  signal control: std_logic_vector(7 downto 0);
 
   component multiplier is
     generic (
@@ -27,16 +29,24 @@ architecture testbench of multiplier_tb is
 begin
   DUT: multiplier
     port map(
-      a => test(7 downto 4),
-      b => test(3 downto 0),
+      a => test_1,
+      b => test_2,
       result => testres
     );
+
+  controlling: process(test_1)
+  begin
+    control <= std_logic_vector(unsigned(test_1) * unsigned(test_2));
+  end process controlling;
 
   testing: process
   begin
     loopz: for i in 0 to 15 loop
-      test <= test +1;
+      test_1 <= test_1 + 1;
+      test_2 <= test_2 + 1;
       wait for 10 ns;
     end loop loopz;
+
+    wait;
   end process testing;
 end testbench;
